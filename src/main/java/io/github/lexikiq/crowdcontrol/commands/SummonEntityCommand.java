71a5a8eef235954414052d95309d1591e43be216
Lexi Larkin <noellekiq@gmail.com>
2021-01-02 17:46:00 -0500

    actually spawn entities on every player

--- src/main/java/io/github/lexikiq/crowdcontrol/commands/SummonEntityCommand.java
+++ src/main/java/io/github/lexikiq/crowdcontrol/commands/SummonEntityCommand.java
@@ -50,10 +50,9 @@ public class SummonEntityCommand extends ChatCommand {
                         spawnEntity(player, loc);
                     }
                 }.runTask(plugin);
-                return true;
             }
         }
-        return false;
+        return true;
     }
 
     protected Entity spawnEntity(Player player, Location location) {
