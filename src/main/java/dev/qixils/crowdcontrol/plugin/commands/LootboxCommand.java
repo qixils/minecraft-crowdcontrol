d53e35949c320bb655ec8c0fd75d81d76876891d
Lexi <lexi@qixils.dev>
2021-08-22 01:50:46 -0400

    Expand java.util imports

--- src/main/java/dev/qixils/crowdcontrol/plugin/commands/LootboxCommand.java
+++ src/main/java/dev/qixils/crowdcontrol/plugin/commands/LootboxCommand.java
@@ -21,7 +21,12 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 public class LootboxCommand extends Command {
