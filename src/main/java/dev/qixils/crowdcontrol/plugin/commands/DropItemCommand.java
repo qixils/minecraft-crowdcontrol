48228b15454db1cf249b39fb126c311b08037074
lexikiq <noellekiq@gmail.com>
2021-08-28 01:38:41 -0400

    Fix DropItemCommand

--- src/main/java/dev/qixils/crowdcontrol/plugin/commands/DropItemCommand.java
+++ src/main/java/dev/qixils/crowdcontrol/plugin/commands/DropItemCommand.java
@@ -5,6 +5,7 @@ import dev.qixils.crowdcontrol.plugin.CrowdControlPlugin;
 import dev.qixils.crowdcontrol.socket.Request;
 import dev.qixils.crowdcontrol.socket.Response;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,8 +22,10 @@ public class DropItemCommand extends Command {
 	public Response.@NotNull Result execute(@NotNull Request request) {
 		Response.Result result = new Response.Result(Response.ResultType.FAILURE, "No players were holding items");
 		for (Player player : CrowdControlPlugin.getPlayers()) {
-			if (player.dropItem(true))
+			if (!player.getInventory().getItemInMainHand().getType().isEmpty()) {
+				Bukkit.getScheduler().runTask(plugin, () -> player.dropItem(true));
 				result = Response.Result.SUCCESS;
+			}
 		}
 		return result;
 	}
