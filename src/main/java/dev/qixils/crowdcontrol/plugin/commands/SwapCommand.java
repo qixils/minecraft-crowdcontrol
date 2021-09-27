89b2eddff7613b1aff9204874e94aad647e69dc0
lexikiq <noellekiq@gmail.com>
2021-09-27 12:29:41 -0400

    Tweak swap command
    May fix reported issues

--- src/main/java/dev/qixils/crowdcontrol/plugin/commands/SwapCommand.java
+++ src/main/java/dev/qixils/crowdcontrol/plugin/commands/SwapCommand.java
@@ -11,6 +11,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -34,10 +35,11 @@ public class SwapCommand extends Command {
         // get shuffled list of players
         Collections.shuffle(players, rand);
         // create a list offset by one
-        List<Player> offset = players.subList(1, players.size());
+        List<Player> offset = new ArrayList<>(players.size());
+        offset.addAll(players.subList(1, players.size()));
         offset.add(players.get(0));
         // get teleport destinations
-        Map<Player, Location> destinations = new HashMap<>();
+        Map<Player, Location> destinations = new HashMap<>(players.size());
         for (int i = 0; i < players.size(); ++i)
             destinations.put(players.get(i), offset.get(i).getLocation());
         // teleport
