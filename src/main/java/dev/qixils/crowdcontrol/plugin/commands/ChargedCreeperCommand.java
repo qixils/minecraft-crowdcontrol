6a7a5f06682621c565e573c25ac86e062cfba1d4
lexikiq <noellekiq@gmail.com>
2021-09-27 14:12:35 -0400

    Properly spawn mobs on top of players

--- src/main/java/dev/qixils/crowdcontrol/plugin/commands/ChargedCreeperCommand.java
+++ src/main/java/dev/qixils/crowdcontrol/plugin/commands/ChargedCreeperCommand.java
@@ -1,15 +1,13 @@
 package dev.qixils.crowdcontrol.plugin.commands;
 
 import dev.qixils.crowdcontrol.plugin.CrowdControlPlugin;
-import dev.qixils.crowdcontrol.plugin.utils.BlockUtil;
 import lombok.Getter;
-import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
-import org.bukkit.entity.AnimalTamer;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 
 @Getter
 public class ChargedCreeperCommand extends SummonEntityCommand {
@@ -21,20 +19,10 @@ public class ChargedCreeperCommand extends SummonEntityCommand {
     private final String displayName = "Summon Charged Creeper";
 
     @Override
-    protected Entity spawnEntity(String viewer, AnimalTamer player, Location location) {
-        Creeper creeper = (Creeper) super.spawnEntity(viewer, player, location);
+    protected Entity spawnEntity(String viewer, Player player) {
+        Creeper creeper = (Creeper) super.spawnEntity(viewer, player);
         creeper.setPowered(true);
-        location.getWorld().playSound(location, Sound.ENTITY_LIGHTNING_BOLT_THUNDER, SoundCategory.HOSTILE, 1.0f, 1.0f);
+        player.getWorld().playSound(player.getLocation(), Sound.ENTITY_LIGHTNING_BOLT_THUNDER, SoundCategory.HOSTILE, 1.0f, 1.0f);
         return creeper;
-    }
-
-    @Override
-    protected Location getSpawnLocation(Location location) {
-        return BlockUtil.blockFinderBuilder()
-                .origin(location)
-                .locationValidator(BlockUtil.SPAWNING_SPACE)
-                .minRadius(5)
-                .maxRadius(SPAWN_RADIUS)
-                .build().next();
     }
 }
--- src/main/java/dev/qixils/crowdcontrol/plugin/commands/SummonEntityCommand.java
+++ src/main/java/dev/qixils/crowdcontrol/plugin/commands/SummonEntityCommand.java
@@ -2,14 +2,11 @@ package dev.qixils.crowdcontrol.plugin.commands;
 
 import dev.qixils.crowdcontrol.plugin.Command;
 import dev.qixils.crowdcontrol.plugin.CrowdControlPlugin;
-import dev.qixils.crowdcontrol.plugin.utils.BlockUtil;
 import dev.qixils.crowdcontrol.plugin.utils.TextUtil;
 import dev.qixils.crowdcontrol.socket.Request;
 import dev.qixils.crowdcontrol.socket.Response;
 import lombok.Getter;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.entity.AnimalTamer;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -33,28 +30,17 @@ public class SummonEntityCommand extends Command {
     @Override
     public Response.@NotNull Result execute(@NotNull Request request) {
         for (Player player : CrowdControlPlugin.getPlayers()) {
-            Location loc = getSpawnLocation(player.getLocation());
-            if (loc != null)
-                Bukkit.getScheduler().runTask(plugin, () -> spawnEntity(request.getViewer(), player, loc));
+            Bukkit.getScheduler().runTask(plugin, () -> spawnEntity(request.getViewer(), player));
         }
         return Response.Result.SUCCESS;
     }
 
-    protected Entity spawnEntity(String viewer, AnimalTamer player, Location location) {
-        Entity entity = location.getWorld().spawnEntity(location, entityType);
+    protected Entity spawnEntity(String viewer, Player player) {
+        Entity entity = player.getWorld().spawnEntity(player.getLocation(), entityType);
         entity.setCustomName(viewer);
         entity.setCustomNameVisible(true);
         if (entity instanceof Tameable tameable)
             tameable.setOwner(player);
         return entity;
-    }
-
-    protected Location getSpawnLocation(Location location) {
-        return BlockUtil.blockFinderBuilder()
-                .origin(location)
-                .locationValidator(BlockUtil.SPAWNING_SPACE)
-                .maxRadius(SPAWN_RADIUS)
-                .shuffleLocations(false)
-                .build().next();
     }
 }
