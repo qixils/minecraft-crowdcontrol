1453de62aaeec2a6d8570eff2d1a016b444c8d22
Lexi <lexi@qixils.dev>
2021-10-21 18:59:43 -0400

    Disallow killing via damage command
    Fixes #18

--- src/main/java/dev/qixils/crowdcontrol/plugin/commands/DamageCommand.java
+++ src/main/java/dev/qixils/crowdcontrol/plugin/commands/DamageCommand.java
@@ -6,6 +6,7 @@ import dev.qixils.crowdcontrol.socket.Request;
 import dev.qixils.crowdcontrol.socket.Response;
 import lombok.Getter;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 @Getter
@@ -23,14 +24,28 @@ public class DamageCommand extends ImmediateCommand {
 
 	@Override
 	public Response.@NotNull Builder executeImmediately(@NotNull Request request) {
-		Bukkit.getScheduler().runTask(plugin, () -> CrowdControlPlugin.getPlayers().forEach(player -> {
-			if (amount < 0)
-				player.setHealth(Math.max(0, Math.min(player.getMaxHealth(), player.getHealth() - amount)));
-			else if (amount >= Short.MAX_VALUE)
-				player.setHealth(0);
-			else
-				player.damage(amount);
-		}));
-		return Response.builder().type(Response.ResultType.SUCCESS);
+		boolean success = false;
+		for (Player player : CrowdControlPlugin.getPlayers()) {
+			if (amount < 0) {
+				success = true;
+				Bukkit.getScheduler().runTask(plugin, () -> player.setHealth(Math.max(0, Math.min(player.getMaxHealth(), player.getHealth() - amount))));
+			} else if (amount >= Short.MAX_VALUE) {
+				success = true;
+				Bukkit.getScheduler().runTask(plugin, () -> player.setHealth(0));
+			} else {
+				double oldHealth = player.getHealth();
+				double newHealth = Math.min(1, oldHealth - amount);
+				double appliedDamage = oldHealth - newHealth;
+				if (appliedDamage > 0) {
+					success = true;
+					Bukkit.getScheduler().runTask(plugin, () -> player.damage(appliedDamage));
+				}
+			}
+		}
+
+		if (success)
+			return Response.builder().type(Response.ResultType.SUCCESS);
+		else
+			return Response.builder().type(Response.ResultType.FAILURE).message("Players would have been killed by this command");
 	}
 }
