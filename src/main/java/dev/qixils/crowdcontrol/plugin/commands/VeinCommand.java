5e05529be656caf2b3dc3b6b408b3efdd706b22a
Lexi <lexi@qixils.dev>
2021-11-18 21:34:37 -0500

    Make ore veins more common + accessible

--- src/main/java/dev/qixils/crowdcontrol/plugin/commands/VeinCommand.java
+++ src/main/java/dev/qixils/crowdcontrol/plugin/commands/VeinCommand.java
@@ -30,14 +30,14 @@ public class VeinCommand extends ImmediateCommand {
         DIAMOND(Material.DIAMOND_ORE, 3),
         IRON(Material.IRON_ORE, 3),
         COAL(Material.COAL_ORE, 3),
-        EMERALD(Material.EMERALD_ORE, 3),
+        EMERALD(Material.EMERALD_ORE, 7),
         GOLD(Material.GOLD_ORE, 3),
         REDSTONE(Material.REDSTONE_ORE, 3),
         LAPIS(Material.LAPIS_ORE, 3),
-        QUARTZ(Material.NETHER_QUARTZ_ORE, 1),
-        NETHER_GOLD(Material.NETHER_GOLD_ORE, 1),
-        SILVERFISH(Material.INFESTED_STONE, 16),
-        LAVA(Material.LAVA, 7)
+        QUARTZ(Material.NETHER_QUARTZ_ORE, 3),
+        NETHER_GOLD(Material.NETHER_GOLD_ORE, 3),
+        SILVERFISH(Material.INFESTED_STONE, 2),
+        LAVA(Material.LAVA, 8)
         ;
 
         private final @Getter Material block;
@@ -50,45 +50,48 @@ public class VeinCommand extends ImmediateCommand {
         public static final int TOTAL_WEIGHTS = Arrays.stream(values()).mapToInt(Ores::getWeight).sum();
     }
 
-    public static final int MIN_RADIUS = 10;
-    public static final int MAX_RADIUS = 20;
+    public static final int MAX_RADIUS = 12;
+    public static final int ORE_VEINS = 2; // ore veins to spawn per player
 
     @Override
     public Response.@NotNull Builder executeImmediately(@NotNull List<@NotNull Player> players, @NotNull Request request) {
-        Material ore = (RandomUtil.weightedRandom(Ores.values(), Ores.TOTAL_WEIGHTS)).getBlock();
-
         Response.Builder result = request.buildResponse().type(Response.ResultType.FAILURE).message("Could not find any blocks to replace");
         for (Player player : players) {
-            List<Location> setBlocks = new ArrayList<>();
-            Location oreLocation = BlockUtil.BlockFinder.builder()
-                    .origin(player.getLocation())
-                    .maxRadius(MAX_RADIUS)
-                    .minRadius(MIN_RADIUS)
-                    .locationValidator(BlockUtil.STONES_TAG::matches)
-                    .build().next();
-            if (oreLocation == null) {
-                continue;
-            }
-            // get 2x2 chunk of blocks
-            for (int x = 0; x <= 1; ++x) {
-                for (int y = 0; y <= 1; ++y) {
-                    for (int z = 0; z <= 1; ++z) {
-                        Location loc = oreLocation.clone().add(x, y, z);
-                        if (BlockUtil.STONES_TAG.matches(loc)) {
-                            setBlocks.add(loc);
+            for (int iter = 0; iter < ORE_VEINS; iter++) {
+                Material ore = RandomUtil.weightedRandom(Ores.values(), Ores.TOTAL_WEIGHTS).getBlock();
+
+                List<Location> setBlocks = new ArrayList<>();
+                Location oreLocation = BlockUtil.BlockFinder.builder()
+                        .origin(player.getLocation())
+                        .maxRadius(MAX_RADIUS)
+                        .locationValidator(BlockUtil.STONES_TAG::matches)
+                        .build().next();
+                if (oreLocation == null) {
+                    continue;
+                }
+                // get 2x2 chunk of blocks
+                for (int x = 0; x <= 1; ++x) {
+                    for (int y = 0; y <= 1; ++y) {
+                        for (int z = 0; z <= 1; ++z) {
+                            Location loc = oreLocation.clone().add(x, y, z);
+                            if (BlockUtil.STONES_TAG.matches(loc)) {
+                                setBlocks.add(loc);
+                            }
                         }
                     }
                 }
-            }
-            // if we found viable blocks (idk how we wouldn't have at least one but just in case??)
-            if (!setBlocks.isEmpty()) {
+
+                // if we didn't find viable blocks, exit
+                if (setBlocks.isEmpty())
+                    continue;
+
                 result.type(Response.ResultType.SUCCESS).message("SUCCESS");
                 List<Location> trueSetBlocks = new ArrayList<>();
-                int maxBlocks = 1+rand.nextInt(setBlocks.size());
+                int maxBlocks = 1 + rand.nextInt(setBlocks.size());
                 int blocksSet = 0;
                 for (int i = 0; i < setBlocks.size() && blocksSet < maxBlocks; ++i) {
-                    int blocksRemaining = maxBlocks-blocksSet;
-                    int iterationsRemaining = setBlocks.size()-i;
+                    int blocksRemaining = maxBlocks - blocksSet;
+                    int iterationsRemaining = setBlocks.size() - i;
                     if (blocksRemaining == iterationsRemaining || rand.nextBoolean()) {
                         ++blocksSet;
                         trueSetBlocks.add(setBlocks.get(i));
