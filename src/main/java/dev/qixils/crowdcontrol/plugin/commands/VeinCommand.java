f35cc43aa05f80f56ad92379b1f6ed008fcf81ee
Lexi <lexi@qixils.dev>
2021-11-20 14:49:13 -0500

    Add deepslate ore support to ore vein effect
    Replaces deepslate blocks with deepslate ores (and anything else with normal ores)
    
    Fixes #20

--- src/main/java/dev/qixils/crowdcontrol/plugin/commands/VeinCommand.java
+++ src/main/java/dev/qixils/crowdcontrol/plugin/commands/VeinCommand.java
@@ -3,6 +3,7 @@ package dev.qixils.crowdcontrol.plugin.commands;
 import dev.qixils.crowdcontrol.plugin.CrowdControlPlugin;
 import dev.qixils.crowdcontrol.plugin.ImmediateCommand;
 import dev.qixils.crowdcontrol.plugin.utils.BlockUtil;
+import dev.qixils.crowdcontrol.plugin.utils.BlockUtil.BlockFinder;
 import dev.qixils.crowdcontrol.plugin.utils.RandomUtil;
 import dev.qixils.crowdcontrol.plugin.utils.Weighted;
 import dev.qixils.crowdcontrol.socket.Request;
@@ -12,10 +13,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Getter
@@ -26,25 +29,33 @@ public class VeinCommand extends ImmediateCommand {
         super(plugin);
     }
 
+    @Getter
     public enum Ores implements Weighted {
-        DIAMOND(Material.DIAMOND_ORE, 3),
-        IRON(Material.IRON_ORE, 3),
-        COAL(Material.COAL_ORE, 3),
-        EMERALD(Material.EMERALD_ORE, 7),
-        GOLD(Material.GOLD_ORE, 3),
-        REDSTONE(Material.REDSTONE_ORE, 3),
-        LAPIS(Material.LAPIS_ORE, 3),
+        DIAMOND(Material.DIAMOND_ORE, Material.DEEPSLATE_DIAMOND_ORE, 3),
+        IRON(Material.IRON_ORE, Material.DEEPSLATE_IRON_ORE, 3),
+        COAL(Material.COAL_ORE, Material.DEEPSLATE_COAL_ORE, 3),
+        EMERALD(Material.EMERALD_ORE, Material.DEEPSLATE_EMERALD_ORE, 7),
+        GOLD(Material.GOLD_ORE, Material.DEEPSLATE_GOLD_ORE, 3),
+        REDSTONE(Material.REDSTONE_ORE, Material.DEEPSLATE_REDSTONE_ORE, 3),
+        LAPIS(Material.LAPIS_ORE, Material.DEEPSLATE_LAPIS_ORE, 3),
         QUARTZ(Material.NETHER_QUARTZ_ORE, 3),
         NETHER_GOLD(Material.NETHER_GOLD_ORE, 3),
-        SILVERFISH(Material.INFESTED_STONE, 2),
+        SILVERFISH(Material.INFESTED_STONE, Material.INFESTED_DEEPSLATE, 2),
         LAVA(Material.LAVA, 8)
         ;
 
-        private final @Getter Material block;
-        private final @Getter int weight;
-        Ores(Material block, int weight) {
+        private final Material block;
+        private final Material deepslateBlock;
+        private final int weight;
+
+        Ores(Material block, Material deepslateBlock, int weight) {
             this.block = block;
+            this.deepslateBlock = deepslateBlock;
             this.weight = weight;
+        }
+
+        Ores(Material block, int weight) {
+            this(block, block, weight);
         }
 
         public static final int TOTAL_WEIGHTS = Arrays.stream(values()).mapToInt(Ores::getWeight).sum();
@@ -57,50 +68,65 @@ public class VeinCommand extends ImmediateCommand {
     public Response.@NotNull Builder executeImmediately(@NotNull List<@NotNull Player> players, @NotNull Request request) {
         Response.Builder result = request.buildResponse().type(Response.ResultType.FAILURE).message("Could not find any blocks to replace");
         for (Player player : players) {
-            for (int iter = 0; iter < ORE_VEINS; iter++) {
-                Material ore = RandomUtil.weightedRandom(Ores.values(), Ores.TOTAL_WEIGHTS).getBlock();
+            BlockFinder finder = BlockFinder.builder()
+                    .origin(player.getLocation())
+                    .maxRadius(MAX_RADIUS)
+                    .locationValidator(BlockUtil.STONES_TAG::matches)
+                    .build();
 
-                List<Location> setBlocks = new ArrayList<>();
-                Location oreLocation = BlockUtil.BlockFinder.builder()
-                        .origin(player.getLocation())
-                        .maxRadius(MAX_RADIUS)
-                        .locationValidator(BlockUtil.STONES_TAG::matches)
-                        .build().next();
-                if (oreLocation == null) {
+            for (int iter = 0; iter < ORE_VEINS; iter++) {
+                Ores ore = RandomUtil.weightedRandom(Ores.values(), Ores.TOTAL_WEIGHTS);
+
+                List<Location> setBlocks = new ArrayList<>(8);
+                List<Location> setDeepslateBlocks = new ArrayList<>(8);
+                Location oreLocation = finder.next();
+                if (oreLocation == null)
                     continue;
-                }
+
                 // get 2x2 chunk of blocks
-                for (int x = 0; x <= 1; ++x) {
-                    for (int y = 0; y <= 1; ++y) {
-                        for (int z = 0; z <= 1; ++z) {
-                            Location loc = oreLocation.clone().add(x, y, z);
-                            if (BlockUtil.STONES_TAG.matches(loc)) {
-                                setBlocks.add(loc);
-                            }
-                        }
-                    }
-                }
+                addOreVein(setDeepslateBlocks, setBlocks, oreLocation);
 
                 // if we didn't find viable blocks, exit
-                if (setBlocks.isEmpty())
+                if (setBlocks.isEmpty() && setDeepslateBlocks.isEmpty())
                     continue;
 
                 result.type(Response.ResultType.SUCCESS).message("SUCCESS");
-                List<Location> trueSetBlocks = new ArrayList<>();
-                int maxBlocks = 1 + rand.nextInt(setBlocks.size());
-                int blocksSet = 0;
-                for (int i = 0; i < setBlocks.size() && blocksSet < maxBlocks; ++i) {
-                    int blocksRemaining = maxBlocks - blocksSet;
-                    int iterationsRemaining = setBlocks.size() - i;
-                    if (blocksRemaining == iterationsRemaining || rand.nextBoolean()) {
-                        ++blocksSet;
-                        trueSetBlocks.add(setBlocks.get(i));
-                    }
-                }
+                randomlyShrinkOreVein(setBlocks);
+                randomlyShrinkOreVein(setDeepslateBlocks);
 
-                Bukkit.getScheduler().runTask(plugin, () -> trueSetBlocks.forEach(blockPos -> blockPos.getBlock().setType(ore)));
+                if (!setBlocks.isEmpty())
+                    Bukkit.getScheduler().runTask(plugin, () -> setBlocks.forEach(blockPos -> blockPos.getBlock().setType(ore.getBlock())));
+                if (!setDeepslateBlocks.isEmpty())
+                    Bukkit.getScheduler().runTask(plugin, () -> setDeepslateBlocks.forEach(blockPos -> blockPos.getBlock().setType(ore.getDeepslateBlock())));
             }
         }
         return result;
+    }
+
+    // Gets a 2x2 chunk of blocks
+    @Contract(value = "null, _, _ -> fail; _, null, _ -> fail; _, _, null -> fail", mutates = "param1, param2")
+    private static void addOreVein(List<Location> deepslateBlocks, List<Location> stoneBlocks, Location base) {
+        for (int x = 0; x <= 1; ++x) {
+            for (int y = 0; y <= 1; ++y) {
+                for (int z = 0; z <= 1; ++z) {
+                    Location loc = base.clone().add(x, y, z);
+                    Material matType = loc.getBlock().getType();
+                    if (matType == Material.DEEPSLATE) {
+                        deepslateBlocks.add(loc);
+                    } else if (BlockUtil.STONES_TAG.matches(matType)) {
+                        stoneBlocks.add(loc);
+                    }
+                }
+            }
+        }
+    }
+
+    @Contract(value = "null -> fail", mutates = "param1")
+    private static void randomlyShrinkOreVein(List<Location> blockLocations) {
+        if (blockLocations.isEmpty()) return;
+        Collections.shuffle(blockLocations, rand);
+        int maxBlocks = 1 + rand.nextInt(blockLocations.size());
+        while (blockLocations.size() > maxBlocks)
+            blockLocations.remove(0);
     }
 }
