cfa726c293d94872a8f874deb3f201da737e7f86
Lexi <lexi@qixils.dev>
2022-02-01 01:09:34 -0500

    Sponge8: Impl PotionCommand

--- sponge-8/src/main/java/dev/qixils/crowdcontrol/plugin/sponge8/commands/PotionCommand.java
+++ sponge-8/src/main/java/dev/qixils/crowdcontrol/plugin/sponge8/commands/PotionCommand.java
@@ -1,7 +1,6 @@
 package dev.qixils.crowdcontrol.plugin.sponge8.commands;
 
 import dev.qixils.crowdcontrol.TimedEffect;
-import dev.qixils.crowdcontrol.common.util.TextUtil;
 import dev.qixils.crowdcontrol.plugin.sponge8.ImmediateCommand;
 import dev.qixils.crowdcontrol.plugin.sponge8.SpongeCrowdControlPlugin;
 import dev.qixils.crowdcontrol.socket.Request;
@@ -9,11 +8,13 @@ import dev.qixils.crowdcontrol.socket.Response;
 import dev.qixils.crowdcontrol.socket.Response.ResultType;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.Keys;
 import org.spongepowered.api.effect.potion.PotionEffect;
 import org.spongepowered.api.effect.potion.PotionEffectType;
 import org.spongepowered.api.effect.potion.PotionEffectTypes;
-import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.server.ServerPlayer;
+import org.spongepowered.api.registry.RegistryTypes;
+import org.spongepowered.api.util.Ticks;
 
 import java.util.Collections;
 import java.util.List;
@@ -23,7 +24,7 @@ public class PotionCommand extends ImmediateCommand {
 	private static final int SECONDS = 15;
 	private static final int TICKS = 20 * SECONDS;
 	private final PotionEffectType potionEffectType;
-	private final int duration;
+	private final Ticks duration;
 	private final String effectName;
 	private final String displayName;
 
@@ -31,15 +32,16 @@ public class PotionCommand extends ImmediateCommand {
 		super(plugin);
 		this.potionEffectType = potionEffectType;
 		boolean isMinimal = potionEffectType.isInstant();
-		duration = isMinimal ? 1 : TICKS;
-		this.effectName = "potion_" + SpongeTextUtil.csIdOf(potionEffectType);
-		this.displayName = "Apply " + TextUtil.titleCase(potionEffectType.getTranslation().get()) + " Potion Effect (" + SECONDS + "s)";
+		duration = Ticks.of(isMinimal ? 1 : TICKS);
+		this.effectName = "potion_" + potionEffectType.key(RegistryTypes.POTION_EFFECT_TYPE).value();
+		// TODO for some reason the sponge 7 impl uses title case for potion effect name; test if this is still necessary
+		this.displayName = "Apply " + plugin.getTextUtil().asPlain(potionEffectType) + " Potion Effect (" + SECONDS + "s)";
 	}
 
 	@NotNull
 	@Override
-	public Response.Builder executeImmediately(@NotNull List<@NotNull Player> players, @NotNull Request request) {
-		if (potionEffectType.equals(PotionEffectTypes.JUMP_BOOST)
+	public Response.Builder executeImmediately(@NotNull List<@NotNull ServerPlayer> players, @NotNull Request request) {
+		if (potionEffectType.equals(PotionEffectTypes.JUMP_BOOST.get())
 				&& TimedEffect.isActive("disable_jumping", request.getTargets())) {
 			return request.buildResponse()
 					.type(ResultType.RETRY)
@@ -50,7 +52,7 @@ public class PotionCommand extends ImmediateCommand {
 				.potionType(potionEffectType)
 				.duration(duration);
 
-		for (Player player : players) {
+		for (ServerPlayer player : players) {
 			PotionEffect effect = builder.build();
 			player.transform(Keys.POTION_EFFECTS, effects -> {
 				if (effects == null)
@@ -59,13 +61,13 @@ public class PotionCommand extends ImmediateCommand {
 				boolean overridden = false;
 				for (int i = 0; i < effects.size(); i++) {
 					PotionEffect existingEffect = effects.get(i);
-					if (existingEffect.getType().equals(potionEffectType)) {
+					if (existingEffect.type().equals(potionEffectType)) {
 						plugin.getSLF4JLogger().debug("Updating existing effect");
 						overridden = true;
 						effects.set(i, PotionEffect.builder()
 								.from(existingEffect)
-								.duration(Math.max(TICKS, existingEffect.getDuration()))
-								.amplifier(existingEffect.getAmplifier() + 1)
+								.duration(Ticks.of(Math.max(TICKS, existingEffect.duration().ticks())))
+								.amplifier(existingEffect.amplifier() + 1)
 								.build());
 						break;
 					}
