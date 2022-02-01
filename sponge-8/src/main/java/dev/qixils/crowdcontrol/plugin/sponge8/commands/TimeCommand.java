eac88706fbb5cea22b309899f6a27cd5d5c3c5c8
Lexi <lexi@qixils.dev>
2022-02-01 01:41:48 -0500

    Sponge8: Impl TimeCommand

--- sponge-8/src/main/java/dev/qixils/crowdcontrol/plugin/sponge8/commands/TimeCommand.java
+++ sponge-8/src/main/java/dev/qixils/crowdcontrol/plugin/sponge8/commands/TimeCommand.java
@@ -1,6 +1,5 @@
 package dev.qixils.crowdcontrol.plugin.sponge8.commands;
 
-import dev.qixils.crowdcontrol.common.CommandConstants;
 import dev.qixils.crowdcontrol.plugin.sponge8.ImmediateCommand;
 import dev.qixils.crowdcontrol.plugin.sponge8.SpongeCrowdControlPlugin;
 import dev.qixils.crowdcontrol.socket.Request;
@@ -8,8 +7,8 @@ import dev.qixils.crowdcontrol.socket.Response;
 import dev.qixils.crowdcontrol.socket.Response.ResultType;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.world.storage.WorldProperties;
+import org.spongepowered.api.entity.living.player.server.ServerPlayer;
+import org.spongepowered.api.world.server.storage.ServerWorldProperties;
 
 import java.util.List;
 
@@ -24,15 +23,15 @@ public class TimeCommand extends ImmediateCommand {
 
 	@NotNull
 	@Override
-	public Response.Builder executeImmediately(@NotNull List<@NotNull Player> players, @NotNull Request request) {
+	public Response.Builder executeImmediately(@NotNull List<@NotNull ServerPlayer> players, @NotNull Request request) {
 		if (!isGlobalCommandUsable(players, request))
 			return request.buildResponse()
 					.type(ResultType.UNAVAILABLE)
 					.message("Global command cannot be used on this streamer");
 
-		sync(() -> plugin.getGame().getServer().getWorlds().forEach(world -> {
-			WorldProperties properties = world.getProperties();
-			properties.setWorldTime(properties.getWorldTime() + CommandConstants.ZIP_TIME_TICKS);
+		sync(() -> plugin.getGame().server().worldManager().worlds().forEach(world -> {
+			ServerWorldProperties properties = world.properties();
+			properties.setDayTime(properties.dayTime().add(0, 12, 0));
 		}));
 		return request.buildResponse().type(Response.ResultType.SUCCESS);
 	}
