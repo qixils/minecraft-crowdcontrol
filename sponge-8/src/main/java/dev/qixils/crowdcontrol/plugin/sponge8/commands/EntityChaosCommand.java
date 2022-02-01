d76996d1284d715e6053cb46e122f19a0b43f8fc
Lexi <lexi@qixils.dev>
2022-01-31 23:08:47 -0500

    Sponge8: Impl EntityChaosCommand

--- sponge-8/src/main/java/dev/qixils/crowdcontrol/plugin/sponge8/commands/EntityChaosCommand.java
+++ sponge-8/src/main/java/dev/qixils/crowdcontrol/plugin/sponge8/commands/EntityChaosCommand.java
@@ -8,12 +8,13 @@ import dev.qixils.crowdcontrol.socket.Response.ResultType;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.api.entity.Entity;
-import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.world.World;
+import org.spongepowered.api.entity.living.player.server.ServerPlayer;
+import org.spongepowered.api.world.server.ServerWorld;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class EntityChaosCommand extends ImmediateCommand {
@@ -26,20 +27,20 @@ public class EntityChaosCommand extends ImmediateCommand {
 
 	@NotNull
 	@Override
-	public Response.Builder executeImmediately(@NotNull List<@NotNull Player> players, @NotNull Request request) {
+	public Response.Builder executeImmediately(@NotNull List<@NotNull ServerPlayer> players, @NotNull Request request) {
 		if (!isGlobalCommandUsable(players, request))
 			return request.buildResponse().type(ResultType.UNAVAILABLE).message("Global command cannot be used on this streamer");
 
 		List<Entity> entities = new ArrayList<>(200);
-		for (World world : plugin.getGame().getServer().getWorlds()) {
-			entities.addAll(world.getEntities(entity -> !entity.getType().equals(EntityTypes.PLAYER)));
+		for (ServerWorld world : plugin.getGame().server().worldManager().worlds()) {
+			entities.addAll(world.entities().stream().filter(entity -> !(entity instanceof Player)).collect(Collectors.toList()));
 		}
 
 		sync(() -> {
 			for (int i = 0; i < entities.size(); i++) {
 				Entity entity = entities.get(i);
-				entity.clearPassengers();
-				entity.setLocation(players.get(i % players.size()).getLocation());
+				// TODO clear passengers
+				entity.setLocation(players.get(i % players.size()).serverLocation());
 			}
 		});
 
