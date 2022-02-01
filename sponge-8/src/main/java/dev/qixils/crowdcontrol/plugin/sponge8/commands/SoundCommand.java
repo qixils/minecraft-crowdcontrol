4e491067ee40b17a048a3aa353cdb2cc586574bd
Lexi <lexi@qixils.dev>
2022-02-01 01:31:53 -0500

    Sponge8: Impl SoundCommand

--- sponge-8/src/main/java/dev/qixils/crowdcontrol/plugin/sponge8/commands/SoundCommand.java
+++ sponge-8/src/main/java/dev/qixils/crowdcontrol/plugin/sponge8/commands/SoundCommand.java
@@ -1,6 +1,5 @@
 package dev.qixils.crowdcontrol.plugin.sponge8.commands;
 
-import com.flowpowered.math.vector.Vector3d;
 import dev.qixils.crowdcontrol.common.util.sound.Sounds;
 import dev.qixils.crowdcontrol.plugin.sponge8.ImmediateCommand;
 import dev.qixils.crowdcontrol.plugin.sponge8.SpongeCrowdControlPlugin;
@@ -8,9 +7,10 @@ import dev.qixils.crowdcontrol.socket.Request;
 import dev.qixils.crowdcontrol.socket.Response;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.data.Keys;
+import org.spongepowered.api.entity.living.player.server.ServerPlayer;
 import org.spongepowered.api.util.Direction;
+import org.spongepowered.math.vector.Vector3d;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,19 +26,14 @@ public class SoundCommand extends ImmediateCommand {
 
 	@NotNull
 	@Override
-	public Response.Builder executeImmediately(@NotNull List<@NotNull Player> players, @NotNull Request request) {
-		for (Player player : players) {
-			Vector3d playAt = player.getPosition();
+	public Response.Builder executeImmediately(@NotNull List<@NotNull ServerPlayer> players, @NotNull Request request) {
+		for (ServerPlayer player : players) {
+			Vector3d playAt = player.position();
 			Optional<Direction> direction = player.get(Keys.DIRECTION);
 			if (direction.isPresent()) {
-				playAt = playAt.add(direction.get().getOpposite().asBlockOffset().toDouble());
+				playAt = playAt.add(direction.get().opposite().asBlockOffset().toDouble());
 			}
-			plugin.asAudience(player).playSound(
-					Sounds.SPOOKY.get(),
-					playAt.getX(),
-					playAt.getY(),
-					playAt.getZ()
-			);
+			player.playSound(Sounds.SPOOKY.get(), playAt);
 		}
 		return request.buildResponse().type(Response.ResultType.SUCCESS);
 	}
