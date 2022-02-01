34ecd6597f777c07c4ab5f286a7624046e4a85d6
Lexi <lexi@qixils.dev>
2022-02-01 00:20:42 -0500

    Sponge8: Impl GravelCommand

--- sponge-8/src/main/java/dev/qixils/crowdcontrol/plugin/sponge8/commands/GravelCommand.java
+++ sponge-8/src/main/java/dev/qixils/crowdcontrol/plugin/sponge8/commands/GravelCommand.java
@@ -7,12 +7,11 @@ import dev.qixils.crowdcontrol.plugin.sponge8.utils.BlockFinder;
 import dev.qixils.crowdcontrol.socket.Request;
 import dev.qixils.crowdcontrol.socket.Response;
 import lombok.Getter;
-import net.kyori.adventure.key.Key;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.api.block.BlockTypes;
-import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.world.Location;
-import org.spongepowered.api.world.World;
+import org.spongepowered.api.entity.living.player.server.ServerPlayer;
+import org.spongepowered.api.registry.RegistryTypes;
+import org.spongepowered.api.world.server.ServerLocation;
 
 import java.util.HashSet;
 import java.util.List;
@@ -29,13 +28,13 @@ public class GravelCommand extends ImmediateCommand {
 
 	@NotNull
 	@Override
-	public Response.Builder executeImmediately(@NotNull List<@NotNull Player> players, @NotNull Request request) {
-		Set<Location<World>> locations = new HashSet<>(50);
-		for (Player player : players)
+	public Response.Builder executeImmediately(@NotNull List<@NotNull ServerPlayer> players, @NotNull Request request) {
+		Set<ServerLocation> locations = new HashSet<>(50);
+		for (ServerPlayer player : players)
 			locations.addAll(BlockFinder.builder()
-					.origin(player.getLocation())
+					.origin(player.serverLocation())
 					.locationValidator(location -> CommonTags.STONES_EXCEPT_GRAVEL.contains(
-							Key.key(location.getBlock().getType().getId())))
+							location.block().type().key(RegistryTypes.BLOCK_TYPE).asString()))
 					.shuffleLocations(false)
 					.maxRadius(6)
 					.build().getAll());
@@ -45,7 +44,7 @@ public class GravelCommand extends ImmediateCommand {
 					.type(Response.ResultType.FAILURE)
 					.message("No replaceable blocks nearby");
 
-		sync(() -> locations.forEach(location -> location.setBlockType(BlockTypes.GRAVEL)));
+		sync(() -> locations.forEach(location -> location.setBlockType(BlockTypes.GRAVEL.get())));
 		return request.buildResponse().type(Response.ResultType.SUCCESS);
 	}
 }
