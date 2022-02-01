1907aed7b67c89f40299102c3589ad37bdcb7ffa
Lexi <lexi@qixils.dev>
2022-02-01 01:35:20 -0500

    Sponge8: Impl TakeItemCommand

--- sponge-8/src/main/java/dev/qixils/crowdcontrol/plugin/sponge8/commands/TakeItemCommand.java
+++ sponge-8/src/main/java/dev/qixils/crowdcontrol/plugin/sponge8/commands/TakeItemCommand.java
@@ -7,16 +7,15 @@ import dev.qixils.crowdcontrol.socket.Response;
 import dev.qixils.crowdcontrol.socket.Response.ResultType;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.server.ServerPlayer;
 import org.spongepowered.api.item.ItemType;
 import org.spongepowered.api.item.ItemTypes;
-import org.spongepowered.api.item.inventory.Carrier;
-import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.api.item.inventory.type.CarriedInventory;
+import org.spongepowered.api.item.inventory.Slot;
+import org.spongepowered.api.item.inventory.entity.PlayerInventory;
+import org.spongepowered.api.registry.RegistryTypes;
 
 import java.util.List;
-import java.util.Optional;
 
 @Getter
 public class TakeItemCommand extends ImmediateCommand {
@@ -27,36 +26,34 @@ public class TakeItemCommand extends ImmediateCommand {
 	public TakeItemCommand(SpongeCrowdControlPlugin plugin, ItemType item) {
 		super(plugin);
 		this.item = item;
-		this.effectName = "take_" + SpongeTextUtil.valueOf(item);
-		this.displayName = "Take " + item.getTranslation().get();
+		this.effectName = "take_" + item.key(RegistryTypes.ITEM_TYPE).value();
+		this.displayName = "Take " + plugin.getTextUtil().asPlain(item);
 	}
 
 	@NotNull
 	@Override
-	public Response.Builder executeImmediately(@NotNull List<@NotNull Player> players, @NotNull Request request) {
+	public Response.Builder executeImmediately(@NotNull List<@NotNull ServerPlayer> players, @NotNull Request request) {
 		Response.Builder response = request.buildResponse()
 				.type(ResultType.RETRY)
 				.message("Item could not be found in target inventories");
-		for (Player player : players) {
-			CarriedInventory<? extends Carrier> inventory = player.getInventory();
-			for (Inventory invSlot : inventory.slots()) {
-				Optional<ItemStack> optionalStack = invSlot.peek();
-				if (!optionalStack.isPresent())
+		for (ServerPlayer player : players) {
+			PlayerInventory inventory = player.inventory();
+			for (Slot invSlot : inventory.slots()) {
+				ItemStack stack = invSlot.peek();
+				if (stack.isEmpty())
 					continue;
-
-				ItemStack stack = optionalStack.get();
-				if (!stack.getType().equals(item))
+				if (!stack.type().equals(item))
 					continue;
 
 				response.type(ResultType.SUCCESS).message("SUCCESS");
 				sync(() -> {
-					stack.setQuantity(stack.getQuantity() - 1);
+					stack.setQuantity(stack.quantity() - 1);
 					invSlot.set(stack);
 				});
 				break;
 			}
 
-			if (ResultType.SUCCESS == response.type() && item.equals(ItemTypes.END_PORTAL_FRAME))
+			if (ResultType.SUCCESS == response.type() && item.equals(ItemTypes.END_PORTAL_FRAME.get()))
 				break;
 		}
 		return response;
