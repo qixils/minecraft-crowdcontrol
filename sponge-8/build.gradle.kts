ea5eae2a431106caff311db60f8bd652cd242f3e
Lexi <lexi@qixils.dev>
2022-06-29 17:33:21 -0400

    chore: bump dependencies
    I'm mad that support for Sponge 8 was not merged into the cloud 1.7 release

--- sponge-8/build.gradle.kts
+++ sponge-8/build.gradle.kts
@@ -4,8 +4,8 @@ description = "Minecraft Crowd Control: Sponge 8"
 
 dependencies {
     implementation(project(":configurate-common"))
-    implementation("cloud.commandframework:cloud-sponge:1.7.0-SNAPSHOT") // TODO use cloudVersion var
-    compileOnly("org.spongepowered:spongeapi:8.0.0")
+    implementation("cloud.commandframework:cloud-sponge:${cloudVersion}")
+    compileOnly("org.spongepowered:spongeapi:8.1.0")
 }
 
 tasks.withType<ProcessResources> {
