fefe095dee173cc825fa2340350beb9c8c218763
Lexi Larkin <noellekiq@gmail.com>
2022-10-26 02:55:35 -0400

    build(deps): bump java-crowd-control from v3.4.0 to v3.4.1
    Bumps [java-crowd-control](https://github.com/qixils/java-crowd-control) from 3.4.0 to 3.4.1.
    - [Release notes](https://github.com/qixils/java-crowd-control/releases)
    - [Commits](qixils/java-crowd-control@v3.4.0...v3.4.1)
    
    Tries and fails to fix an issue with KyoriTranslator on :paper-platform:runServer

--- common/src/main/java/dev/qixils/crowdcontrol/common/KyoriTranslator.java
+++ common/src/main/java/dev/qixils/crowdcontrol/common/KyoriTranslator.java
@@ -31,7 +31,7 @@ public class KyoriTranslator {
 		translator.defaultLocale(Locale.US);
 
 		// find locale path
-		URL url = KyoriTranslator.class.getResource("/i18n/");
+		URL url = KyoriTranslator.class.getResource("/i18n");
 		File dir = getFileFromURL(url);
 		if (dir == null)
 			throw new IllegalStateException("Could not load language files (directory does not exist)");
