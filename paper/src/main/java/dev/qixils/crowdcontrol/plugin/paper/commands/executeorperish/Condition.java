b517086622cf7730f64a19f9885f1dfff127f8b9
Lexi Larkin <noellekiq@gmail.com>
2022-10-27 17:40:54 -0400

    feat(paper): more dimensional do-or-die

--- paper/src/main/java/dev/qixils/crowdcontrol/plugin/paper/commands/executeorperish/Condition.java
+++ paper/src/main/java/dev/qixils/crowdcontrol/plugin/paper/commands/executeorperish/Condition.java
@@ -3,6 +3,7 @@ package dev.qixils.crowdcontrol.plugin.paper.commands.executeorperish;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,28 +13,7 @@ import java.util.List;
 @Getter
 public enum Condition {
 	// stand on...
-	STAND_ON_DIRT(new StandOnBlockCondition(0, "generic", Material.DIRT)),
-	STAND_ON_STONE(new StandOnBlockCondition(0, "generic", Material.STONE)),
 	STAND_ON_COBBLESTONE(new StandOnBlockCondition(0, "generic", Material.COBBLESTONE)),
-	STAND_ON_SAND(new StandOnBlockCondition(1, "generic", Material.SAND)),
-	STAND_ON_A_BED(new StandOnBlockCondition(2, "bed",
-			Material.WHITE_BED,
-			Material.ORANGE_BED,
-			Material.MAGENTA_BED,
-			Material.LIGHT_BLUE_BED,
-			Material.YELLOW_BED,
-			Material.LIME_BED,
-			Material.PINK_BED,
-			Material.GRAY_BED,
-			Material.LIGHT_GRAY_BED,
-			Material.CYAN_BED,
-			Material.PURPLE_BED,
-			Material.BLUE_BED,
-			Material.BROWN_BED,
-			Material.GREEN_BED,
-			Material.RED_BED,
-			Material.BLACK_BED
-	)),
 	STAND_ON_A_PLANK(new StandOnBlockCondition(0, "plank",
 			Material.OAK_PLANKS,
 			Material.BIRCH_PLANKS,
@@ -44,7 +24,7 @@ public enum Condition {
 			Material.DARK_OAK_PLANKS,
 			Material.SPRUCE_PLANKS
 	)),
-	STAND_ON_A_STRIPPED_LOG(new StandOnBlockCondition(2, "stripped_log",
+	STAND_ON_A_STRIPPED_LOG(new StandOnBlockCondition(4, "stripped_log",
 			Material.STRIPPED_OAK_LOG,
 			Material.STRIPPED_BIRCH_LOG,
 			Material.STRIPPED_ACACIA_LOG,
@@ -63,14 +43,56 @@ public enum Condition {
 			Material.STRIPPED_WARPED_HYPHAE
 	)),
 	// obtain...
-	OBTAIN_WHEAT_SEEDS(new ObtainItemCondition(0, "generic", Material.WHEAT_SEEDS)),
 	OBTAIN_STONE(new ObtainItemCondition(3, "generic_block", Material.STONE)),
 	// craft...
 	CRAFT_STONE_HOE(new CraftItemCondition(1, "generic", Material.STONE_HOE)),
 	CRAFT_WOODEN_HOE(new CraftItemCondition(0, "generic", Material.WOODEN_HOE)),
+
+	//// overworld-only ////
+	// stand on...
+	STAND_ON_DIRT(new StandOnBlockCondition(0, "generic", Material.DIRT, ConditionFlags.OVERWORLD)),
+	STAND_ON_STONE(new StandOnBlockCondition(0, "generic", Material.STONE, ConditionFlags.OVERWORLD)),
+	STAND_ON_SAND(new StandOnBlockCondition(1, "generic", Material.SAND, ConditionFlags.OVERWORLD)),
+	STAND_ON_A_BED(new StandOnBlockCondition(3, "bed",
+			Material.WHITE_BED,
+			ConditionFlags.OVERWORLD, // hi
+			Material.ORANGE_BED,
+			Material.MAGENTA_BED,
+			Material.LIGHT_BLUE_BED,
+			Material.YELLOW_BED,
+			Material.LIME_BED,
+			Material.PINK_BED,
+			Material.GRAY_BED,
+			Material.LIGHT_GRAY_BED,
+			Material.CYAN_BED,
+			Material.PURPLE_BED,
+			Material.BLUE_BED,
+			Material.BROWN_BED,
+			Material.GREEN_BED,
+			Material.RED_BED,
+			Material.BLACK_BED
+	)),
+	// obtain...
+	OBTAIN_WHEAT_SEEDS(new ObtainItemCondition(0, "generic_alt", Material.WHEAT_SEEDS, ConditionFlags.OVERWORLD)),
+	// craft...
+	CRAFT_SANDSTONE(new CraftItemCondition(0, "generic_block", Material.SANDSTONE, ConditionFlags.OVERWORLD)),
+
+	//// nether-only ////
+	// stand on...
+	STAND_ON_FIRE(new StandOnBlockCondition(0, "generic", Material.FIRE, ConditionFlags.NETHER)),
+	// obtain...
+	OBTAIN_NETHER_BRICK(new ObtainItemCondition(3, "generic", Material.NETHER_BRICK, ConditionFlags.NETHER)),
+	OBTAIN_OBSIDIAN(new ObtainItemCondition(2, "generic_alt", Material.OBSIDIAN, ConditionFlags.NETHER)),
+	// craft...
+	CRAFT_QUARTZ(new CraftItemCondition(0, "generic", Material.QUARTZ_BLOCK, ConditionFlags.NETHER)),
+	CRAFT_GOLD_INGOT(new CraftItemCondition(0, "generic", Material.GOLD_INGOT, ConditionFlags.NETHER)),
+	CRAFT_GLOWSTONE(new CraftItemCondition(0, "generic_block", Material.GLOWSTONE, ConditionFlags.NETHER)),
+
+	//// end-only ////
+	OBTAIN_IRON_BARS(new ObtainItemCondition(0, "generic_alt", Material.IRON_BARS, ConditionFlags.THE_END)),
 	;
 
-	private static final List<SuccessCondition> CONDITIONS;
+	private static final @NotNull List<SuccessCondition> CONDITIONS;
 
 	static {
 		List<SuccessCondition> conditions = new ArrayList<>(values().length);
@@ -79,9 +101,9 @@ public enum Condition {
 		CONDITIONS = Collections.unmodifiableList(conditions);
 	}
 
-	private final SuccessCondition condition;
+	private final @NotNull SuccessCondition condition;
 
-	public static List<SuccessCondition> items() {
+	public static @NotNull List<@NotNull SuccessCondition> items() {
 		return CONDITIONS;
 	}
 }
--- paper/src/main/java/dev/qixils/crowdcontrol/plugin/paper/commands/executeorperish/ConditionFlags.java
+++ paper/src/main/java/dev/qixils/crowdcontrol/plugin/paper/commands/executeorperish/ConditionFlags.java
@@ -6,33 +6,35 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
+
+import static java.util.Collections.singletonList;
 
 @Data
 @Builder
 public final class ConditionFlags implements Predicate<Player> {
 	public static final @NotNull ConditionFlags DEFAULT = ConditionFlags.builder().build();
+	public static final @NotNull ConditionFlags OVERWORLD = ConditionFlags.builder().allowedDimensions(singletonList(World.Environment.NORMAL)).build();
+	public static final @NotNull ConditionFlags NETHER = ConditionFlags.builder().allowedDimensions(singletonList(World.Environment.NETHER)).build();
+	public static final @NotNull ConditionFlags THE_END = ConditionFlags.builder().allowedDimensions(singletonList(World.Environment.THE_END)).build();
 
 	@Builder.Default
-	private final @Nullable List<World.Environment> allowedDimensions = null;
+	private final @NotNull List<World.Environment> allowedDimensions = Collections.emptyList();
 	@Builder.Default
-	private final @Nullable Collection<Material> requiredItems = null;
+	private final @NotNull Collection<Material> requiredItems = Collections.emptyList();
 
-	@SuppressWarnings("ConstantConditions") // IntelliJ thinks the parameters are non-null for some reason
 	@Override
 	public boolean test(@NotNull Player player) {
-		if (allowedDimensions != null && !allowedDimensions.contains(player.getWorld().getEnvironment()))
+		if (!allowedDimensions.isEmpty() && !allowedDimensions.contains(player.getWorld().getEnvironment()))
 			return false;
-		if (requiredItems != null) {
-			for (Material material : requiredItems) {
-				// TODO: does this check armor & offhand (and does it matter if it doesn't?)
-				if (!player.getInventory().contains(material))
-					return false;
-			}
+		for (Material material : requiredItems) {
+			// TODO: does this check armor & offhand (and does it matter if it doesn't?)
+			if (!player.getInventory().contains(material))
+				return false;
 		}
 		return true;
 	}
