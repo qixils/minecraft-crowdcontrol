aa654fa92f527040cbfcdedce0eb46fdaa512244
Lexi Larkin <lexi@qixils.dev>
2023-03-16 23:59:09 -0400

    fix: bump fabric.mod.json Minecraft version

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -54,10 +54,12 @@ dependencies {
 
 tasks.processResources {
     inputs.property("version", project.version)
+    inputs.property("minecraftVersion", minecraftVersion)
     filteringCharset = "UTF-8"
 
     filesMatching("fabric.mod.json") {
         expand("version" to project.version)
+        expand("minecraftVersion" to minecraftVersion)
     }
 }
 
