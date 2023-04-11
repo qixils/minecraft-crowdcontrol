7559d2e4cea132a457d5f4bffab3cdb2badf6d20
Lexi Larkin <lexi@qixils.dev>
2023-04-10 22:35:37 -0400

    chore: update to yarn build 5
    yarn has a few mappings for some of the new classes but they just don't work??? like the auto migrator picked them up but they don't show up anywhere in either the decompiled .class files or the generated .java files, makes no sense. loom i guess isn't handling records well but i dont see an open issue for it so IDK

--- fabric/src/main/java/dev/qixils/crowdcontrol/plugin/fabric/mixin/ProposalScreenMixin.java
+++ fabric/src/main/java/dev/qixils/crowdcontrol/plugin/fabric/mixin/ProposalScreenMixin.java
@@ -3,7 +3,7 @@ package dev.qixils.crowdcontrol.plugin.fabric.mixin;
 import dev.qixils.crowdcontrol.plugin.fabric.FabricCrowdControlPlugin;
 import dev.qixils.crowdcontrol.plugin.fabric.client.FabricPlatformClient;
 import net.minecraft.class_8373;
-import net.minecraft.class_8448;
+import net.minecraft.client.gui.screen.ingame.vote.VotingScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,9 +11,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(class_8448.class)
+@Mixin(VotingScreen.class)
 public class ProposalScreenMixin {
-	@Shadow private class_8448.class_8452 field_44364;
+	@Shadow private VotingScreen.class_8452 field_44364;
 
 	@Inject(method = "init", at = @At("RETURN"))
 	private void onInit(CallbackInfo ci) {
