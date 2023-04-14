a104b1a6754b9edd18e42739bfdc64ae36b53a77
Lexi Larkin <lexi@qixils.dev>
2023-04-13 20:45:22 -0400

    chore: reduce durations

--- fabric/src/main/java/dev/qixils/crowdcontrol/plugin/fabric/client/ProposalVote.java
+++ fabric/src/main/java/dev/qixils/crowdcontrol/plugin/fabric/client/ProposalVote.java
@@ -15,8 +15,8 @@ import java.util.*;
 
 public class ProposalVote {
 	public static final int MIN_DURATION = 20 * 20;
-	public static final int MAX_DURATION = 60 * 20;
-	public static final int COOLDOWN = 10 * 20;
+	public static final int MAX_DURATION = 50 * 20;
+	public static final int COOLDOWN = 7 * 20;
 	/**
 	 * How many ticks to subtract from the time remaining on a proposal to ensure our vote gets sent on time.
 	 */
