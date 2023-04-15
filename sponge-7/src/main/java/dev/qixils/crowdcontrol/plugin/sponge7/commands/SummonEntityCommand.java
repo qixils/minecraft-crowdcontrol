e3bf009dc4ca65fd09e1a4eb2af261bdbdb5617d
Lexi Larkin <lexi@qixils.dev>
2023-04-14 22:40:07 -0400

    try and fail to fix sponge7 armor stand equipment

--- sponge-7/src/main/java/dev/qixils/crowdcontrol/plugin/sponge7/commands/SummonEntityCommand.java
+++ sponge-7/src/main/java/dev/qixils/crowdcontrol/plugin/sponge7/commands/SummonEntityCommand.java
@@ -13,6 +13,7 @@ import net.kyori.adventure.translation.GlobalTranslator;
 import org.jetbrains.annotations.Blocking;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.data.type.DyeColor;
 import org.spongepowered.api.data.type.RabbitType;
@@ -29,7 +30,7 @@ import org.spongepowered.api.event.cause.entity.spawn.SpawnTypes;
 import org.spongepowered.api.item.ItemType;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.equipment.EquipmentType;
-import org.spongepowered.api.item.inventory.equipment.HeldEquipmentType;
+import org.spongepowered.api.item.inventory.equipment.EquipmentTypes;
 import org.spongepowered.api.item.inventory.property.EquipmentSlotType;
 
 import java.util.*;
@@ -40,7 +41,15 @@ import static dev.qixils.crowdcontrol.common.util.RandomUtil.*;
 
 @Getter
 public class SummonEntityCommand extends ImmediateCommand implements EntityCommand {
-	private final Map<EquipmentType, List<ItemType>> armor;
+	private static final Map<EquipmentType, List<ItemType>> ARMOR;
+	private static final List<EquipmentType> NON_ARMOR = Arrays.asList(
+			EquipmentTypes.MAIN_HAND,
+			EquipmentTypes.OFF_HAND,
+			EquipmentTypes.HELD,
+			EquipmentTypes.EQUIPPED,
+			EquipmentTypes.ANY,
+			EquipmentTypes.WORN // TODO: handle this
+	);
 	protected final EntityType entityType;
 	private final String effectName;
 	private final Component displayName;
@@ -56,25 +65,17 @@ public class SummonEntityCommand extends ImmediateCommand implements EntityComma
 		variants.put(RabbitTypes.WHITE, 16);
 		variants.put(RabbitTypes.KILLER, 1);
 		RABBIT_VARIANTS = Collections.unmodifiableMap(variants);
-	}
-
-	public SummonEntityCommand(SpongeCrowdControlPlugin plugin, EntityType entityType) {
-		super(plugin);
-		this.entityType = entityType;
-		this.effectName = "entity_" + SpongeTextUtil.csIdOf(entityType);
-		this.displayName = Component.translatable("cc.effect.summon_entity.name", SpongeTextUtil.getFixedName(entityType));
 
 		// pre-compute the map of valid armor pieces
 		Map<EquipmentType, List<ItemType>> armor = new HashMap<>(4);
-		for (ItemType item : plugin.getRegistry().getAllOf(ItemType.class)) {
+		for (ItemType item : Sponge.getRegistry().getAllOf(ItemType.class)) {
 			Optional<EquipmentType> optionalType = item
-					.getDefaultProperty(EquipmentSlotType.class)
+					.getDefaultProperty(EquipmentSlotType.class) // TODO: is this not implemented??
 					.map(EquipmentSlotType::getValue);
 			if (!optionalType.isPresent()) continue;
 
 			EquipmentType type = optionalType.get();
-			if (type instanceof HeldEquipmentType)
-				continue;
+			if (NON_ARMOR.contains(type)) continue;
 
 			armor.computeIfAbsent(type, $ -> new ArrayList<>()).add(item);
 		}
@@ -82,7 +83,14 @@ public class SummonEntityCommand extends ImmediateCommand implements EntityComma
 		// make collections unmodifiable
 		for (Map.Entry<EquipmentType, List<ItemType>> entry : new HashSet<>(armor.entrySet()))
 			armor.put(entry.getKey(), Collections.unmodifiableList(entry.getValue()));
-		this.armor = Collections.unmodifiableMap(armor);
+		ARMOR = Collections.unmodifiableMap(armor);
+	}
+
+	public SummonEntityCommand(SpongeCrowdControlPlugin plugin, EntityType entityType) {
+		super(plugin);
+		this.entityType = entityType;
+		this.effectName = "entity_" + SpongeTextUtil.csIdOf(entityType);
+		this.displayName = Component.translatable("cc.effect.summon_entity.name", SpongeTextUtil.getFixedName(entityType));
 	}
 
 	@NotNull
@@ -143,7 +151,7 @@ public class SummonEntityCommand extends ImmediateCommand implements EntityComma
 			// could add some chaos (GH#64) here eventually
 			// chaos idea: set drop chance for each slot to a random float
 			//             (not that this is in API v7 afaik)
-			List<EquipmentType> slots = new ArrayList<>(armor.keySet());
+			List<EquipmentType> slots = new ArrayList<>(ARMOR.keySet());
 			Collections.shuffle(slots, random);
 			// begins as a 1 in 4 chance to add a random item but becomes less likely each time
 			// an item is added
@@ -152,7 +160,7 @@ public class SummonEntityCommand extends ImmediateCommand implements EntityComma
 				if (random.nextInt(odds) > 0)
 					continue;
 				odds += ENTITY_ARMOR_INC;
-				ItemStack item = ItemStack.of(randomElementFrom(armor.get(type)));
+				ItemStack item = ItemStack.of(randomElementFrom(ARMOR.get(type)));
 				plugin.commandRegister().getCommandByName("lootbox", LootboxCommand.class)
 						.randomlyModifyItem(item, odds / ENTITY_ARMOR_START);
 				((ArmorEquipable) entity).equip(type, item);
