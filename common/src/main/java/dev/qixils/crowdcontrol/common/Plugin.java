b82b4c05838b5451f6b61cf1bd6162a8d2d79cdc
Lexi Larkin <lexi@qixils.dev>
2023-05-08 15:52:22 -0400

    fix #133

--- common/src/main/java/dev/qixils/crowdcontrol/common/Plugin.java
+++ common/src/main/java/dev/qixils/crowdcontrol/common/Plugin.java
@@ -254,9 +254,8 @@ public interface Plugin<P, S> {
 
 		// base command
 		Builder<S> account = manager.commandBuilder("account")
-				.meta(CommandMeta.DESCRIPTION, "Manage your connected Twitch account(s)");
-		if (isAdminRequired())
-			account = account.permission(mapper::isAdmin);
+				.meta(CommandMeta.DESCRIPTION, "Manage your connected Twitch account(s)")
+				.permission(sender -> !isAdminRequired() || mapper.isAdmin(sender));
 
 		// link command
 		manager.command(account.literal("link")
