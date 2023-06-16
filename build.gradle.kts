b54074ab8b4af3592f13a492311b8a60a5db2353
Lexi Larkin <lexi@qixils.dev>
2023-06-15 21:02:21 -0400

    chore: bump dependencies
    bump fabric loader to minimum required by fabric-api, and bump adventure-platform-fabric for local lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ subprojects {
                 url = uri("https://files.minecraftforge.net/maven/")
             }
         }
+        mavenLocal() // TODO: remove once adventure-platform-fabric is published
     }
 
     java {
