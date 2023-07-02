6e70829d486f11ba838819a5de0b3fc3480c2fe4
Lexi Larkin <lexi@qixils.dev>
2023-07-02 19:10:26 -0400

    feat: safer command registering

--- fabric/src/main/java/dev/qixils/crowdcontrol/plugin/fabric/CommandRegister.java
+++ fabric/src/main/java/dev/qixils/crowdcontrol/plugin/fabric/CommandRegister.java
@@ -18,7 +18,6 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.level.block.Block;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class CommandRegister extends AbstractCommandRegister<ServerPlayer, FabricCrowdControlPlugin> {
@@ -44,109 +43,109 @@ public class CommandRegister extends AbstractCommandRegister<ServerPlayer, Fabri
 	protected void createCommands(List<Command<ServerPlayer>> commands) {
 		super.createCommands(commands);
 		registerTags();
-		commands.addAll(Arrays.asList(
-				new VeinCommand(plugin),
-				new SoundCommand(plugin),
-				new ChargedCreeperCommand(plugin),
-				new SwapCommand(plugin),
-				new DinnerboneCommand(plugin),
-				new ClutterCommand(plugin),
-				new LootboxCommand(plugin, 0),
-				new LootboxCommand(plugin, 5),
-				new LootboxCommand(plugin, 10),
-				new TeleportCommand(plugin),
-				new ToastCommand(plugin),
-				FreezeCommand.feet(plugin),
-				FreezeCommand.camera(plugin),
-				FreezeCommand.skyCamera(plugin),
-				FreezeCommand.groundCamera(plugin),
-				new FlowerCommand(plugin),
-				new FlingCommand(plugin),
-				new TorchCommand(plugin, true),
-				new TorchCommand(plugin, false),
-				new GravelCommand(plugin),
-				new DigCommand(plugin),
-				new ItemDurabilityCommand.Repair(plugin),
-				new ItemDurabilityCommand.Damage(plugin),
-				new RemoveEnchantsCommand(plugin),
-				new HatCommand(plugin),
-				new RespawnCommand(plugin),
-				new DropItemCommand(plugin),
-				new DeleteItemCommand(plugin),
-				new BucketClutchCommand(plugin),
-				MovementStatusCommand.disableJumping(plugin),
-				MovementStatusCommand.invertControls(plugin),
-				MovementStatusCommand.invertCamera(plugin),
-				new EntityChaosCommand(plugin),
-				new FlightCommand(plugin),
-				new KeepInventoryCommand(plugin, true),
-				new KeepInventoryCommand(plugin, false),
-				new ClearInventoryCommand(plugin),
-				new PlantTreeCommand(plugin),
-				new DoOrDieCommand(plugin),
-				new ExplodeCommand(plugin),
-				SetTimeCommand.day(plugin),
-				SetTimeCommand.night(plugin),
-				WeatherCommand.clear(plugin),
-				WeatherCommand.downfall(plugin),
-				WeatherCommand.storm(plugin),
-				GravityCommand.zero(plugin),
-				GravityCommand.low(plugin),
-				GravityCommand.high(plugin),
-				GravityCommand.maximum(plugin),
-				new BiomeCommand(plugin),
-				new StructureCommand(plugin),
-				new ShaderCommand(plugin, "bumpy", new SemVer(3, 3, 0)),
-				new ShaderCommand(plugin, "green", new SemVer(3, 3, 0)),
-				new ShaderCommand(plugin, "ntsc", new SemVer(3, 3, 0)),
-				new ShaderCommand(plugin, "desaturate", new SemVer(3, 3, 0)),
-				new ShaderCommand(plugin, "flip", new SemVer(3, 3, 0)),
-				new ShaderCommand(plugin, "invert", new SemVer(3, 3, 0)),
-				new ShaderCommand(plugin, "blobs2", new SemVer(3, 3, 0)),
-				new ShaderCommand(plugin, "pencil", new SemVer(3, 3, 0)),
-				new ShaderCommand(plugin, "sobel", new SemVer(3, 3, 0)),
-				new ShaderCommand(plugin, "cc_wobble", new SemVer(3, 3, 0)),
-				new ShaderCommand(plugin, "bits", new SemVer(3, 3, 0)),
-				new ShaderCommand(plugin, "spider", new SemVer(3, 3, 0)),
-				new ShaderCommand(plugin, "phosphor", new SemVer(3, 3, 0)),
-				new DeleteRandomItemCommand(plugin)
+		commands.addAll(this.<Command<ServerPlayer>>initAll(
+			() -> new VeinCommand(plugin),
+			() -> new SoundCommand(plugin),
+			() -> new ChargedCreeperCommand(plugin),
+			() -> new SwapCommand(plugin),
+			() -> new DinnerboneCommand(plugin),
+			() -> new ClutterCommand(plugin),
+			() -> new LootboxCommand(plugin, 0),
+			() -> new LootboxCommand(plugin, 5),
+			() -> new LootboxCommand(plugin, 10),
+			() -> new TeleportCommand(plugin),
+			() -> new ToastCommand(plugin),
+			() -> FreezeCommand.feet(plugin),
+			() -> FreezeCommand.camera(plugin),
+			() -> FreezeCommand.skyCamera(plugin),
+			() -> FreezeCommand.groundCamera(plugin),
+			() -> new FlowerCommand(plugin),
+			() -> new FlingCommand(plugin),
+			() -> new TorchCommand(plugin, true),
+			() -> new TorchCommand(plugin, false),
+			() -> new GravelCommand(plugin),
+			() -> new DigCommand(plugin),
+			() -> new ItemDurabilityCommand.Repair(plugin),
+			() -> new ItemDurabilityCommand.Damage(plugin),
+			() -> new RemoveEnchantsCommand(plugin),
+			() -> new HatCommand(plugin),
+			() -> new RespawnCommand(plugin),
+			() -> new DropItemCommand(plugin),
+			() -> new DeleteItemCommand(plugin),
+			() -> new BucketClutchCommand(plugin),
+			() -> MovementStatusCommand.disableJumping(plugin),
+			() -> MovementStatusCommand.invertControls(plugin),
+			() -> MovementStatusCommand.invertCamera(plugin),
+			() -> new EntityChaosCommand(plugin),
+			() -> new FlightCommand(plugin),
+			() -> new KeepInventoryCommand(plugin, true),
+			() -> new KeepInventoryCommand(plugin, false),
+			() -> new ClearInventoryCommand(plugin),
+			() -> new PlantTreeCommand(plugin),
+			() -> new DoOrDieCommand(plugin),
+			() -> new ExplodeCommand(plugin),
+			() -> SetTimeCommand.day(plugin),
+			() -> SetTimeCommand.night(plugin),
+			() -> WeatherCommand.clear(plugin),
+			() -> WeatherCommand.downfall(plugin),
+			() -> WeatherCommand.storm(plugin),
+			() -> GravityCommand.zero(plugin),
+			() -> GravityCommand.low(plugin),
+			() -> GravityCommand.high(plugin),
+			() -> GravityCommand.maximum(plugin),
+			() -> new BiomeCommand(plugin),
+			() -> new StructureCommand(plugin),
+			() -> new ShaderCommand(plugin, "bumpy", new SemVer(3, 3, 0)),
+			() -> new ShaderCommand(plugin, "green", new SemVer(3, 3, 0)),
+			() -> new ShaderCommand(plugin, "ntsc", new SemVer(3, 3, 0)),
+			() -> new ShaderCommand(plugin, "desaturate", new SemVer(3, 3, 0)),
+			() -> new ShaderCommand(plugin, "flip", new SemVer(3, 3, 0)),
+			() -> new ShaderCommand(plugin, "invert", new SemVer(3, 3, 0)),
+			() -> new ShaderCommand(plugin, "blobs2", new SemVer(3, 3, 0)),
+			() -> new ShaderCommand(plugin, "pencil", new SemVer(3, 3, 0)),
+			() -> new ShaderCommand(plugin, "sobel", new SemVer(3, 3, 0)),
+			() -> new ShaderCommand(plugin, "cc_wobble", new SemVer(3, 3, 0)),
+			() -> new ShaderCommand(plugin, "bits", new SemVer(3, 3, 0)),
+			() -> new ShaderCommand(plugin, "spider", new SemVer(3, 3, 0)),
+			() -> new ShaderCommand(plugin, "phosphor", new SemVer(3, 3, 0)),
+			() -> new DeleteRandomItemCommand(plugin)
 		));
 
 		// entity commands
 		for (EntityType<?> entity : BuiltInRegistries.ENTITY_TYPE) {
-			commands.add(new SummonEntityCommand<>(plugin, entity));
-			commands.add(new RemoveEntityCommand<>(plugin, entity));
+			initTo(commands, () -> new SummonEntityCommand<>(plugin, entity));
+			initTo(commands, () -> new RemoveEntityCommand<>(plugin, entity));
 		}
 
 		// register difficulty commands
 		for (Difficulty difficulty : Difficulty.values())
-			commands.add(new DifficultyCommand(plugin, difficulty));
+			initTo(commands, () -> new DifficultyCommand(plugin, difficulty));
 
 		// potions
 		for (MobEffect potion : BuiltInRegistries.MOB_EFFECT)
-			commands.add(new PotionCommand(plugin, potion));
+			initTo(commands, () -> new PotionCommand(plugin, potion));
 
 		// block sets
 		for (Block block : setBlocks)
-			commands.add(new BlockCommand(plugin, block));
+			initTo(commands, () -> new BlockCommand(plugin, block));
 
 		for (Block block : setFallingBlocks)
-			commands.add(new FallingBlockCommand(plugin, block));
+			initTo(commands, () -> new FallingBlockCommand(plugin, block));
 
 		// enchantments
 		for (Enchantment enchantment : BuiltInRegistries.ENCHANTMENT)
-			commands.add(new EnchantmentCommand(plugin, enchantment));
+			initTo(commands, () -> new EnchantmentCommand(plugin, enchantment));
 
 		// give/take items
 		for (Item item : giveTakeItems) {
-			commands.add(new GiveItemCommand(plugin, item));
-			commands.add(new TakeItemCommand(plugin, item));
+			initTo(commands, () -> new GiveItemCommand(plugin, item));
+			initTo(commands, () -> new TakeItemCommand(plugin, item));
 		}
 
 		// gamemode commands
 		for (GameType gameType : GameType.values()) {
 			if (gameType == GameType.SURVIVAL) continue;
-			commands.add(new GameModeCommand(plugin, gameType, gameType == GameType.SPECTATOR ? 8L : 15L));
+			initTo(commands, () -> new GameModeCommand(plugin, gameType, gameType == GameType.SPECTATOR ? 8L : 15L));
 		}
 	}
 
--- paper/src/main/java/dev/qixils/crowdcontrol/plugin/paper/CommandRegister.java
+++ paper/src/main/java/dev/qixils/crowdcontrol/plugin/paper/CommandRegister.java
@@ -16,7 +16,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.potion.PotionEffectType;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static dev.qixils.crowdcontrol.common.command.CommandConstants.DAY;
@@ -34,83 +33,83 @@ public class CommandRegister extends AbstractCommandRegister<Player, PaperCrowdC
 	@Override
 	protected void createCommands(List<Command<Player>> commands) {
 		super.createCommands(commands);
-		commands.addAll(Arrays.asList(
-				new VeinCommand(plugin),
-				new SoundCommand(plugin),
-				new ChargedCreeperCommand(plugin),
-				new SwapCommand(plugin),
-				new DinnerboneCommand(plugin),
-				new ClutterCommand(plugin),
-				new LootboxCommand(plugin, 0),
-				new LootboxCommand(plugin, 5),
-				new LootboxCommand(plugin, 10),
-				new TeleportCommand(plugin),
-				new ToastCommand(plugin),
-				new FreezeCommand(plugin),
-				new CameraLockCommand(plugin),
-				new FlowerCommand(plugin),
-				new FlingCommand(plugin),
-				new TorchCommand(plugin, true),
-				new TorchCommand(plugin, false),
-				new GravelCommand(plugin),
-				new DigCommand(plugin),
-				new ItemDamageCommand(plugin, true),
-				new ItemDamageCommand(plugin, false),
-				new RemoveEnchantsCommand(plugin),
-				new HatCommand(plugin),
-				new RespawnCommand(plugin),
-				new DropItemCommand(plugin),
-				new DeleteItemCommand(plugin),
-				new BucketClutchCommand(plugin),
-				new DisableJumpingCommand(plugin),
-				new EntityChaosCommand(plugin),
-				new CameraLockToSkyCommand(plugin),
-				new CameraLockToGroundCommand(plugin),
-				new FlightCommand(plugin),
-				new KeepInventoryCommand(plugin, true),
-				new KeepInventoryCommand(plugin, false),
-				new ClearInventoryCommand(plugin),
-				new PlantTreeCommand(plugin),
-				new DoOrDieCommand(plugin),
-				new ClearWeatherCommand(plugin),
-				new RainyWeatherCommand(plugin),
-				new ThunderingWeatherCommand(plugin),
-				new StructureCommand(plugin),
-				new BiomeCommand(plugin),
-				new ExplodeCommand(plugin),
-				new SetTimeCommand(plugin, "time_day", DAY),
-				new SetTimeCommand(plugin, "time_night", NIGHT),
-				GravityCommand.zero(plugin),
-				GravityCommand.low(plugin),
-				GravityCommand.high(plugin),
-				GravityCommand.maximum(plugin),
-				new DeleteRandomItemCommand(plugin)
+		commands.addAll(this.<Command<Player>>initAll(
+			() -> new VeinCommand(plugin),
+			() -> new SoundCommand(plugin),
+			() -> new ChargedCreeperCommand(plugin),
+			() -> new SwapCommand(plugin),
+			() -> new DinnerboneCommand(plugin),
+			() -> new ClutterCommand(plugin),
+			() -> new LootboxCommand(plugin, 0),
+			() -> new LootboxCommand(plugin, 5),
+			() -> new LootboxCommand(plugin, 10),
+			() -> new TeleportCommand(plugin),
+			() -> new ToastCommand(plugin),
+			() -> new FreezeCommand(plugin),
+			() -> new CameraLockCommand(plugin),
+			() -> new FlowerCommand(plugin),
+			() -> new FlingCommand(plugin),
+			() -> new TorchCommand(plugin, true),
+			() -> new TorchCommand(plugin, false),
+			() -> new GravelCommand(plugin),
+			() -> new DigCommand(plugin),
+			() -> new ItemDamageCommand(plugin, true),
+			() -> new ItemDamageCommand(plugin, false),
+			() -> new RemoveEnchantsCommand(plugin),
+			() -> new HatCommand(plugin),
+			() -> new RespawnCommand(plugin),
+			() -> new DropItemCommand(plugin),
+			() -> new DeleteItemCommand(plugin),
+			() -> new BucketClutchCommand(plugin),
+			() -> new DisableJumpingCommand(plugin),
+			() -> new EntityChaosCommand(plugin),
+			() -> new CameraLockToSkyCommand(plugin),
+			() -> new CameraLockToGroundCommand(plugin),
+			() -> new FlightCommand(plugin),
+			() -> new KeepInventoryCommand(plugin, true),
+			() -> new KeepInventoryCommand(plugin, false),
+			() -> new ClearInventoryCommand(plugin),
+			() -> new PlantTreeCommand(plugin),
+			() -> new DoOrDieCommand(plugin),
+			() -> new ClearWeatherCommand(plugin),
+			() -> new RainyWeatherCommand(plugin),
+			() -> new ThunderingWeatherCommand(plugin),
+			() -> new StructureCommand(plugin),
+			() -> new BiomeCommand(plugin),
+			() -> new ExplodeCommand(plugin),
+			() -> new SetTimeCommand(plugin, "time_day", DAY),
+			() -> new SetTimeCommand(plugin, "time_night", NIGHT),
+			() -> GravityCommand.zero(plugin),
+			() -> GravityCommand.low(plugin),
+			() -> GravityCommand.high(plugin),
+			() -> GravityCommand.maximum(plugin),
+			() -> new DeleteRandomItemCommand(plugin)
 		));
 
 		// entity commands
 		for (EntityType entity : EntityType.values()) {
 			if (entity == EntityType.UNKNOWN) continue;
-			commands.add(new SummonEntityCommand(plugin, entity));
-			commands.add(new RemoveEntityCommand(plugin, entity));
+			initTo(commands, () -> new SummonEntityCommand(plugin, entity));
+			initTo(commands, () -> new RemoveEntityCommand(plugin, entity));
 		}
 
 		// register difficulty commands
 		for (Difficulty difficulty : Difficulty.values()) {
-			commands.add(new DifficultyCommand(plugin, difficulty));
+			initTo(commands, () -> new DifficultyCommand(plugin, difficulty));
 		}
 
 		// potions
 		for (PotionEffectType potionEffectType : PotionEffectType.values()) {
-			commands.add(new PotionCommand(plugin, potionEffectType));
+			initTo(commands, () -> new PotionCommand(plugin, potionEffectType));
 		}
 
 		// block sets
 		for (Material block : SET_BLOCKS) {
-			commands.add(new BlockCommand(plugin, block));
+			initTo(commands, () -> new BlockCommand(plugin, block));
 		}
 
 		for (Material block : SET_FALLING_BLOCKS) {
-			commands.add(new FallingBlockCommand(plugin, block));
+			initTo(commands, () -> new FallingBlockCommand(plugin, block));
 		}
 
 		// enchantments
@@ -124,15 +123,14 @@ public class CommandRegister extends AbstractCommandRegister<Player, PaperCrowdC
 
 		// give/take items
 		for (Material item : GIVE_TAKE_ITEMS) {
-			commands.add(new GiveItemCommand(plugin, item));
-			commands.add(new TakeItemCommand(plugin, item));
+			initTo(commands, () -> new GiveItemCommand(plugin, item));
+			initTo(commands, () -> new TakeItemCommand(plugin, item));
 		}
 
 		// gamemode commands
 		for (GameMode gamemode : GameMode.values()) {
 			if (gamemode == GameMode.SURVIVAL) continue;
-			commands.add(new GameModeCommand(plugin, gamemode,
-					gamemode == GameMode.SPECTATOR ? 8L : 15L)); // duration (in seconds)
+			initTo(commands, () -> new GameModeCommand(plugin, gamemode, gamemode == GameMode.SPECTATOR ? 8L : 15L));
 		}
 	}
 
