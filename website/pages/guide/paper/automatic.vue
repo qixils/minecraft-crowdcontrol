b839b6abf865e0c6c0d6d98b9dffb5bcf3f1335e
Lexi Larkin <lexi@qixils.dev>
2023-09-08 20:42:47 -0400

    feat: note ports in oneclick setup

--- website/pages/guide/paper/automatic.vue
+++ website/pages/guide/paper/automatic.vue
@@ -22,6 +22,12 @@ const version = paperLatest;
       <li>In Minecraft, add the server <code>localhost</code> to your multiplayer server list and join it.</li>
       <li>(Optional) If you're sharing effects with another streamer, run the command <code>/account link INSERT_USERNAME</code> to receive their effects. The username should come from what's displayed in the top left corner of their Crowd Control app.</li>
       <li>Select <strong>Start Session</strong> in the Crowd Control app.</li>
+      <li>(Optional) To allow friends to connect to your server, you will need to port forward.
+        <ul>
+          <li>Open the port <strong>25565</strong> to allow players to join the Minecraft server.</li>
+          <li>Open the port <strong>58431</strong> to allow players to connect their Crowd Control app to the server.</li>
+        </ul>
+      </li>
     </ol>
   </div>
 </template>
