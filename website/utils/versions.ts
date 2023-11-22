3e663d253e770b37735bc736ce9cae447cc824c1
Lexi Larkin <lexi@qixils.dev>
2023-11-22 15:43:48 -0500

    feat: update website versions

--- website/utils/versions.ts
+++ website/utils/versions.ts
@@ -1,7 +1,7 @@
 export const paperVersions: string[] = ["1.20.2", "1.20.1", "1.19.4"];
 export const paperLatest = paperVersions[0];
-export const supportedFabricVersions: string[] = ["1.20.2"];
-export const fabricVersions: string[] = ["1.20.2", "1.20.1", "1.19.4", "1.19.2"];
+export const supportedFabricVersions: string[] = ["1.20.3"];
+export const fabricVersions: string[] = ["1.20.3", "1.20.2", "1.20.1", "1.19.4", "1.19.2"];
 export const fabricLatest = fabricVersions[0];
 export const spongeVersions: Map<string, number> = new Map([["1.16.5", 8], ["1.12.2", 7]]);
 export const spongeVersionsArray: string[] = Array.from(spongeVersions.keys());
