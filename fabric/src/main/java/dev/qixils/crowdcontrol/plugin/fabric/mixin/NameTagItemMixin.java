7c973e0578b74546c43d5a0459b6ae76b9a17f9c
Lexi Larkin <lexi@qixils.dev>
2024-05-31 13:07:47 -0400

    fix: item tag mixin

--- fabric/src/main/java/dev/qixils/crowdcontrol/plugin/fabric/mixin/NameTagItemMixin.java
+++ fabric/src/main/java/dev/qixils/crowdcontrol/plugin/fabric/mixin/NameTagItemMixin.java
@@ -2,19 +2,26 @@ package dev.qixils.crowdcontrol.plugin.fabric.mixin;
 
 import dev.qixils.crowdcontrol.plugin.fabric.interfaces.Components;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.NameTagItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(NameTagItem.class)
-public class NameTagItemMixin {
-	@Inject(method = "interactLivingEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getDisplayName()Lnet/minecraft/network/chat/Component;"))
-	private void injected(ItemStack itemStack, Player player, LivingEntity livingEntity, InteractionHand interactionHand, CallbackInfo ci) {
-		Components.VIEWER_MOB.get(livingEntity).setViewerSpawned(false);
+public abstract class NameTagItemMixin extends Item {
+
+	public NameTagItemMixin(Properties properties) {
+		super(properties);
+	}
+
+	@Inject(method = "interactLivingEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;shrink(I)V"))
+	private void onInteractLivingEntity(ItemStack itemStack, Player player, LivingEntity livingEntity, InteractionHand interactionHand, CallbackInfoReturnable<InteractionResult> cir) {
+		Components.VIEWER_MOB.maybeGet(livingEntity).ifPresent(mob -> mob.setViewerSpawned(false));
 	}
 }
