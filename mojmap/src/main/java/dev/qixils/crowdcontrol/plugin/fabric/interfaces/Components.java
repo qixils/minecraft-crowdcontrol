03fc8dd30a90a3557fb89f44db2521b103457747
Lexi Larkin <lexi@qixils.dev>
2026-03-24 08:30:38 -0400

    fix: key

--- mojmap/src/main/java/dev/qixils/crowdcontrol/plugin/fabric/interfaces/Components.java
+++ mojmap/src/main/java/dev/qixils/crowdcontrol/plugin/fabric/interfaces/Components.java
@@ -1,10 +1,10 @@
 package dev.qixils.crowdcontrol.plugin.fabric.interfaces;
 
-import net.minecraft.resources.ResourceLocation;
+import net.kyori.adventure.key.Key;
 import org.jetbrains.annotations.NotNull;
 
 public final class Components {
-	public static final @NotNull String VIEWER_MOB = new ResourceLocation("crowdcontrol", "viewer-mob").asString();
-	public static final @NotNull String ORIGINAL_DISPLAY_NAME = new ResourceLocation("crowdcontrol", "original-display-name").asString();
-	public static final @NotNull String GAME_TYPE_EFFECT = new ResourceLocation("crowdcontrol", "game-type-effect").asString();
+	public static final @NotNull String VIEWER_MOB = Key.key("crowdcontrol", "viewer-mob").asString();
+	public static final @NotNull String ORIGINAL_DISPLAY_NAME = Key.key("crowdcontrol", "original-display-name").asString();
+	public static final @NotNull String GAME_TYPE_EFFECT = Key.key("crowdcontrol", "game-type-effect").asString();
 }
